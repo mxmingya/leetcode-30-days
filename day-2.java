3d9d9878fe8a532c2cc400ac80fc18fa1d099392
Ming <mxmingya@gmail.com>
2020-04-02 22:50:53 -0700

    Create day-2.java

---
+++ day-2.java
@@ -0,0 +1,28 @@
+class day-2 {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        if (n == 1) {
+            return true;
+        }
+        while (n > 1) {
+            int temp = 0;
+            while (n > 0) {
+                int cur = n % 10;
+                temp += cur * cur;
+                n /= 10;
+            }
+            if (temp == 1) {
+                return true;
+            }
+            if (set.contains(temp)) {
+                return false;
+            } else {
+                set.add(temp);
+                n = temp;
+            }
+            
+        }
+        return false;
+    }
+    
+}
