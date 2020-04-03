6212aff93c86a20c4dafe23c494c1d59613cc787
Ming <mxmingya@gmail.com>
2020-04-03 14:56:17 -0700

    Create day-3.java

---
+++ day-3.java
@@ -0,0 +1,17 @@
+class day-3 {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int curMax = nums[0], res = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (curMax < 0) {
+                curMax = 0;
+            }
+            curMax = curMax + nums[i];
+            res = Math.max(res, curMax);
+        }
+        return res;
+    }
+}
