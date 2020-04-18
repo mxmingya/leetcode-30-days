d3e1e6605ed7753a33f8a7731a1e856044189a09
Ming <mxmingya@gmail.com>
2020-04-18 10:08:26 -0700

    Create Day18-2.java

---
+++ Day18-2.java
@@ -0,0 +1,24 @@
+class Day18-2 {
+// 1 dimentional dynamic programming
+    public int minPathSum(int[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        dp[0] = grid[0][0];
+        for (int i = 1;  i < n; i++) {
+            dp[i] = grid[0][i] + dp[i-1];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[j] = dp[j] + grid[i][j];
+                } else {
+                    dp[j] = Math.min(dp[j], dp[j-1]) + grid[i][j];    
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
