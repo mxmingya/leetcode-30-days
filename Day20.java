4c28edf5a9807d9ae0fe79bab477d50e58b153be
Ming <mxmingya@gmail.com>
2020-04-20 20:53:57 -0700

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Day20 {
+   int i = 0;
+    public TreeNode bstFromPreorder(int[] A) {
+        return bstFromPreorder(A, Integer.MAX_VALUE);
+    }
+
+    public TreeNode bstFromPreorder(int[] A, int bound) {
+        if (i == A.length || A[i] > bound) return null;
+        TreeNode root = new TreeNode(A[i]);
+        // System.out.printf("root: %d, bound: %d \n", A[i], bound);
+        i++;
+        root.left = bstFromPreorder(A, root.val);
+        root.right = bstFromPreorder(A, bound);
+        return root;
+    }
+}
